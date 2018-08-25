ebebc901da72079c501b09697e3f5f6675cac18f
Mrlgm <877205619@qq.com>
2018-08-25 14:26:55 +0800

    点击歌曲激活

--- src/js/new-song.js
+++ src/js/new-song.js
@@ -17,9 +17,16 @@
       window.eventHub.on('upload', (data) => {
         this.active()
       })
+      window.eventHub.on('select', (data) => {
+        console.log(data)
+        this.deactive()
+      })
     },
-    active(){
+    active() {
       $(this.view.el).addClass('active')
+    },
+    deactive() {
+      $(this.view.el).removeClass('active')
     }
   }
   controller.init(view, model)
--- src/js/song-list.js
+++ src/js/song-list.js
@@ -10,7 +10,7 @@
       let songs = data.songs
       let liList = songs.map((song) => {
         let li = $('<li></li>')
-        li.text(song.name + '-' + song.singer)
+        li.text(song.name + '-' + song.singer).attr('data-id', song.id)
         return li
       })
       $el.find('ul').empty()
@@ -21,6 +21,10 @@
     },
     clearActive() {
       $(this.el).find('.active').removeClass('active')
+    },
+    activeItem(li) {
+      let $li = $(li)
+      $li.addClass('active').siblings('.active').removeClass('active')
     }
   }
 
@@ -50,14 +54,28 @@
       this.view = view
       this.model = model
       this.view.render(this.model.data)
+      this.bindEventHub()
+      this.bindEvents()
+      this.getAllSongs()
+    },
+    getAllSongs() {
+      this.model.find().then(() => {
+        this.view.render(this.model.data)
+      })
+    },
+    bindEvents(e) {
+      $(this.view.el).on('click', 'li', (e) => {
+        this.view.activeItem(e.currentTarget)
+        let songId = e.currentTarget.getAttribute('data-id')
+        window.eventHub.triger('select', songId)
+      })
+    },
+    bindEventHub() {
       window.eventHub.on('upload', () => {
         this.view.clearActive()
       })
       window.eventHub.on('create', (songData) => {
         this.model.data.songs.push(songData)
-        this.view.render(this.model.data)
-      })
-      this.model.find().then(() => {
         this.view.render(this.model.data)
       })
     }
